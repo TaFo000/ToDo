14e2b84b3d02078623c08271b63b9700b2a70372
tanya <tanuceerveevna@gmail.com>
2022-09-23 23:50:15 +0300

    zzz

--- lib/MainScreen.dart
+++ lib/MainScreen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
 
@@ -9,44 +8,135 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-
   Color color = Colors.grey;
   bool checkedValue = false;
   bool newValue = false;
 
   @override
   Widget build(BuildContext context) {
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.blue;
+      }
+      return Colors.grey;
+    }
+
+    int _counter = 0;
+
+    void _incrementCounter() {
+      setState(() {
+        _counter++;
+      });
+    }
+
     return Scaffold(
-        
-        backgroundColor: Colors.black45,
-
-        body:
-        Container(
-
-          child: Center(
-
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(50.0),
-                child: Container(
-                  child: Column(
-                    children: [
-                      Text("Мои дела"),
-                      Row(
-                        children: [
-                          Text("Выполнено — 5"),
-                          Icon(
-                            Icons.visibility
-                          )
-                        ],
-                      )
-                    ],
+      backgroundColor: Colors.black38,
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: Container(
+          child: SingleChildScrollView(
+            child: Container(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(60, 82, 154, 6),
+                    child: Text(
+                      "Мои дела",
+                      style: TextStyle(fontSize: 32, color: Colors.white),
+                    ),
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(60, 0, 24, 18),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Выполнено — 5",
+                          style: TextStyle(fontSize: 16, color: Colors.grey),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(120, 0, 0, 0),
+                          child: Icon(
+                            Icons.visibility,
+                            color: Colors.blue,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.grey,
+                      ),
+                      child: Column(
+                        children: [
+                          ListTile(
+                            leading: Checkbox(
+                              checkColor: Colors.white,
+                              fillColor:
+                                  MaterialStateProperty.resolveWith(getColor),
+                              value: checkedValue,
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  checkedValue = value!;
+                                });
+                              },
+                            ),
+                            title: Text("Купить что-то"),
+                            trailing: Icon(Icons.info),
+                          ),
+                          ListTile(
+                            leading: Checkbox(
+                              checkColor: Colors.white,
+                              fillColor:
+                                  MaterialStateProperty.resolveWith(getColor),
+                              value: checkedValue,
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  checkedValue = value!;
+                                });
+                              },
+                            ),
+                            title: Text("Купить что-то"),
+                            trailing: Icon(Icons.info),
+                          ),
+                          ListTile(
+                            leading: Checkbox(
+                              checkColor: Colors.white,
+                              fillColor:
+                                  MaterialStateProperty.resolveWith(getColor),
+                              value: checkedValue,
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  checkedValue = value!;
+                                });
+                              },
+                            ),
+                            title: Text("Купить что-то"),
+                            trailing: Icon(Icons.info),
+                          ),
+                          ListTile(
+                            title: Text("Новое"),
+                          ),
+                        ],
+                      ))
+                ],
               ),
             ),
           ),
-        )
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
